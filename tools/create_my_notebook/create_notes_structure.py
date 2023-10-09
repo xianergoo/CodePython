c18c2b5062c3d34f3b44927c71c91d6c7b6b396d
zhou <xianergoo@gmail.com>
2023-10-09 22:49:34 +0800

    init fluent python note file with create scipt

--- tools/create_my_notebook/create_notes_structure.py
+++ tools/create_my_notebook/create_notes_structure.py
@@ -1,12 +1,26 @@
 import os
 import json
 
+DEFAULT_BASE_PATH = '../docs'
+
 def create_note_struct(file_name):
-    # if not os.path.exists(file_name):
-    #     os.mkdir(file_name)
+   
     with open(file_name, 'r', encoding='utf-8') as f:
-        chapters = json.load(f)
-        for chapter in chapters['chapters']:
+        notes = json.load(f)
+
+        note_name = notes['note_name']
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        parent_path = os.path.dirname(current_dir)
+
+        # 如果当前note文文件夹不存在则创建
+        note_base_path = os.path.join(parent_path, DEFAULT_BASE_PATH)
+        note_path = os.path.join(note_base_path, note_name)
+    
+        if not os.path.exists(note_path):
+            os.mkdir(note_path)
+
+        for chapter in notes['chapters']:
             chapter_number = chapter['chapter_number']
             chapter_title = chapter['chapter_title']
             chapter_filename = chapter['file_name']
@@ -16,25 +30,21 @@ def create_note_struct(file_name):
             python_file_name = f'{chapter_filename}.py'
             
             # 创建章节编号文件夹
-            if not os.path.exists(folder_name):
-                pass
-                # os.makedirs(folder_name)
+            chapter_path = os.path.join(note_path, folder_name)
+            if not os.path.exists(chapter_path):
+                os.makedirs(chapter_path)
             
             # 创建空的Markdown文件
-            markdown_file_path = os.path.join(folder_name, markdown_file_name)
+            markdown_file_path = os.path.join(chapter_path, markdown_file_name)
             if not os.path.exists(markdown_file_path):
-                # with open(markdown_file_path, 'w', encoding='utf-8') as md_file:
-                #     md_file.write('')
-                pass
+                with open(markdown_file_path, 'w', encoding='utf-8') as md_file:
+                    md_file.write('')
             
             # 创建空的Python文件
-            python_file_path = os.path.join(folder_name, python_file_name)
+            python_file_path = os.path.join(chapter_path, python_file_name)
             if not os.path.exists(python_file_path):
-                # with open(python_file_path, 'w', encoding='utf-8') as py_file:
-                #     py_file.write('')
-                pass
-            
-            print(f'Chapter {chapter_number}: Created folder, markdown file, and python file.')
+                with open(python_file_path, 'w', encoding='utf-8') as py_file:
+                    py_file.write('')
 
 if __name__ == '__main__':
     create_note_struct('fluent_python_note.json')
